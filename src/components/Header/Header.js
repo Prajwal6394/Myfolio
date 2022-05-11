176d77ccc38185cf2f02e5f2ecb3ae90684a28c6
prajwal6394 <prajwalpandey2009@gmail.com>
2022-05-12 02:15:16 +0530

    resume link added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -25,7 +25,7 @@ const Header = ({ toggle }) => {
         <NavBtn>
           <a
             className="btn PrimaryBtn"
-            href="https://github.com/prajwal6394/"
+            href="https://drive.google.com/file/d/10EOkNG4METXKuQfNddIb28UDDkYwPGUX/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
           >
